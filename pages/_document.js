0c4b509db19fb86dcef0136b95c9021139a927f4
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-09-19 15:46:08 +0200

    small adjustments

--- pages/_document.js
+++ pages/_document.js
@@ -3,24 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
-          rel="stylesheet"
-        />   
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fff"/>
-        <meta name="msapplication-TileColor" content="#fff"/>
-        <meta name="theme-color" content="#ffffff"></meta>
-        <link rel="stylesheet" href="https://use.typekit.net/txa4ybu.css"/>
-        <script async src="https://eocampaign1.com/form/e621049e-0aba-11ee-a2d7-fb653a89d3ac.js" data-form="e621049e-0aba-11ee-a2d7-fb653a89d3ac"></script>
+      <Head> 
       </Head>
       <body className="overflow-x-hidden antialiased">
         <Main />
