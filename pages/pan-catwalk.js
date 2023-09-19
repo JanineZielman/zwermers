8dea0a6890ba2876cbafdfb1afec52e9d0d135d6
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-09-19 17:28:13 +0200

    test

---
+++ pages/pan-catwalk.js
@@ -0,0 +1,40 @@
+import { SliceZone } from "@prismicio/react";
+import { components } from "../slices";
+
+import { createClient } from "../prismicio";
+import { Layout } from "../components/Layout";
+import Link from "next/link";
+import { PrismicRichText } from "@prismicio/react";
+
+import Type from "../components/type"
+
+const PanCatwalk = ({ menu, page }) => {
+  const letters = page.data.title?.[0]?.text.split('');
+
+  return (
+    <Layout navItems={menu.data.slices}>
+      <div className="wrapper-pan-catwalk wrapper">
+        <Type letters={letters}/>
+        <img className="test" src="test.png"/>
+        <SliceZone slices={page.data.slices} components={components} />
+      </div>
+    </Layout>
+  );
+};
+
+export default PanCatwalk;
+
+export async function getStaticProps({ previewData }) {
+  const client = createClient({ previewData });
+
+  const menu = await client.getSingle("menu");
+  const page = await client.getByUID("page", "pan-catwalk");
+
+
+  return {
+    props: {
+      menu,
+      page
+    },
+  };
+}
