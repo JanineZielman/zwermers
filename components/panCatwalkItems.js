4ebe3141e5506afac6bb873c59db28ce40f9d1b7
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2023-12-15 12:48:37 +0100

    update

--- components/panCatwalkItems.js
+++ components/panCatwalkItems.js
@@ -4,14 +4,18 @@ import Isotope from 'isotope-layout';
 
 const PanCatwalkItems = ({items, page, labels}) => {
 
-  console.log(labels)
+  // console.log(labels)
 
-  function func(a, b) {  
-    // return 0.5 - Math.random();
-  } 
+  // function func(a, b) {  
+  //   return 0.5 - Math.random();
+  // } 
 
   const isotope = useRef()
   const [filterKey, setFilterKey] = useState('*')
+
+  const [matchItems, setMatchItems] = useState(null);
+  const [match, setMatch] = useState(null);
+  
 
   // initialize an Isotope object with configs
   useEffect(() => {
@@ -25,29 +29,61 @@ const PanCatwalkItems = ({items, page, labels}) => {
 
   // handling filter key change
   useEffect(() => {
-    filterKey === '*'
+     filterKey === '*'
       ? isotope.current.arrange({filter: `*`})
-      : isotope.current.arrange({filter: `.${filterKey}`})
+      : isotope.current.arrange({filter: `${filterKey.replace('*', '')}`})
+
+    if(filterKey != '*'){
+      setMatchItems(document.querySelectorAll(filterKey));
+      // console.log(match)
+      for (let i = 0; i < document.querySelectorAll(filterKey).length; i++) {
+        if(document.querySelectorAll(filterKey)[i].classList.value.includes(filterKey.replace('*', '').replaceAll('.', ' '))){
+          setMatch(match + document.querySelectorAll(filterKey)[i].classList.value.replaceAll('.', ' '));
+        }
+      }
+    } else {
+      setMatch(null);
+      setMatchItems(null);
+    }
   }, [filterKey])
 
-  const handleFilterKeyChange = key => () => setFilterKey(key)
+  console.log(match)
+
+
+
+  const handleFilterKeyChange = key => () => {
+    if(key === '*'){
+      setFilterKey(key);
+    } else{
+      if(filterKey.includes(key)){
+        setFilterKey(filterKey.replace(key, ''));
+        document.getElementById(key).classList.remove('active');
+      } else {
+        setFilterKey(filterKey + key);
+        document.getElementById(key).classList.add('active');
+      }
+    }
+    
+  }
 
   return (
     <div className='pan-catwalk-page'>
       <div className='filter'>
         <div className='label' onClick={handleFilterKeyChange('*')}>Show all</div>
-        {labels.slice(0,25).map((label, i) => {
-          let active = false;
-          if (label.uid == filterKey){
-            active = true;
-          }
+        {labels.map((label, i) => {
+          
+          console.log(label.uid)
           return(
-            <div key={`label${i}`} className={`label ${label.data.category} ${active}`} onClick={handleFilterKeyChange(label.uid)}>{label.uid.replaceAll('-', ' ')}</div>
+            <>
+            {match?.includes(label.uid) || matchItems == null &&
+              <div id={`.${label.uid}`} key={`label${i}`} className={`label ${label.data.category}`} onClick={handleFilterKeyChange('.' + label.uid)}>{label.uid.replaceAll('-', ' ')}</div>
+            }
+            </>
           )
         })}
       </div>
       <div className='pan-catwalk-items'>
-        {items.sort(func).map((item, i) => {  
+        {items.map((item, i) => {  
           let labelsclasses = [];
           for (let i = 0; i < item.data.labels.length; i++) {
             labelsclasses += item.data.labels[i].label.uid + ' '
